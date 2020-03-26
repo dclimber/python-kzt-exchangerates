cf3463ba56d420ffe7474bfd368169d8d284cee5
dclimber <dastand.climber@gmail.com>
2020-03-26 15:09:40 +0600

    feat: Update API to only get exchange rates for KZT.
    It was possible to get rates for any supported currency,
    by calculating the rate through KZT.
    For example USD to KZT = {usd_rate},
    GBP to KZT = {gbp_rate}, so it was producing USD to GBP
    rate by {usd_rate}/{gbp_rate}, which is correct math,
    but Economy does not work that way!
    
    So now such methods are removed, because wrong rates
    were produced.

--- example.py
+++ example.py
@@ -2,25 +2,24 @@ from kzt_exchangerates import Rates
 
 rates = Rates()
 
-print('Latest exchange rates for KZT:\n%s' % rates.get_exchange_rates())
-print('\nLatest exchange rates for USD:\n%s' % rates.get_exchange_rates('USD'))
+print('Latest exchange rates for KZT (1 KZT = ...):\n%s' %
+      rates.get_exchange_rates())
+print('\nLatest exchange rates from KZT (... = ... KZT):\n%s' %
+      rates.get_exchange_rates(from_kzt=True))
 print(
-    '\nExchange rates for RUB on 12.01.2019:\n%s' %
-    rates.get_exchange_rates('RUB', date="12.01.2019")
+  '\nExchange rates for KZT and USD, EUR, GBP for 12.01.2019 (1 KZT = ...):\n%s' %
+  rates.get_exchange_rates(['USD', 'EUR', 'GBP'], date='12.01.2019')
 )
 print(
-    '\nExchange rates for USD and KZT,EUR,GBP for 12.01.2019:\n%s' %
-    rates.get_exchange_rates('USD', ['KZT', 'EUR', 'GBP'], date='12.01.2019')
-)
-print(
-    '\nCurrent exchange rates for USD and required currencies:\n%s' %
-    rates.get_exchange_rates('USD', ['CAD', 'EUR', 'GBP', 'AUD', 'RUB'])
+  '\nExchange rates for USD, EUR from KZT for 12.01.2019 (... = ... KZT):\n%s' %
+  rates.get_exchange_rates(['USD', 'EUR'], from_kzt=True, date='12.01.2019')
 )
 print('\nCurrently 1 KZT = %s %s' %
-      (rates.get_exchange_rate('KZT', 'USD'), 'USD'))
+      (rates.get_exchange_rate('USD'), 'USD'))
 print('\nCurrently 1 USD = %s %s' %
-      (rates.get_exchange_rate('USD', 'KZT'), 'KZT'))
+      (rates.get_exchange_rate('USD', from_kzt=True), 'KZT'))
+print('\n1 GBP = %s %s' %
+      (rates.get_exchange_rate('GBP', from_kzt=True, date='12.01.2019'),
+       'KZT on 12.01.2019'))
 print('\n1 KZT = %s %s' %
-      (rates.get_exchange_rate('KZT', 'USD', '12.01.2019'), 'USD on 12.01.2019'))
-print('\n1 EUR = %s %s' %
-      (rates.get_exchange_rate('EUR', 'RUB', '12.01.2007'), 'RUB on 12.01.2019'))
+      (rates.get_exchange_rate('GBP', date='12.01.2019'), 'GBP on 12.01.2019'))
--- kzt_exchangerates/rates.py
+++ kzt_exchangerates/rates.py
@@ -80,83 +80,59 @@ class Rates(object):
         return self.rss_url.format(
                 endpoint=self.rss_endpoints[self.LATEST])
 
-    def _calculate_through_kzt(self, base, target, rates):
-        """Method to calculate exchange rate through exchange
-        rates for Kazakhstani Tenge.
-
-        Args:
-            param1 (obj): self
-            param2 (str): base currency
-            param3 (str): target currency
-            param4 (dict): KZT exchange rates dictionary
-
-        Returns:
-            float()
-        """
-        if target == self.KZT_CODE:
-            return rates[base]
-        elif base == self.KZT_CODE:
-            return rates[target]
-        else:
-            base_to_med = rates[base]
-            target_to_med = rates[target]
-            return base_to_med / target_to_med
-
-    def _get_rates_for_currency(self, base, target_list, rates):
-        """Method for getting exchange rates for given currency
+    def _get_rates_for_targets(self, from_kzt, target_list, rates):
+        """Method for getting exchange rates for given currencies
         based on Kazakhstani Tenge exchange rates.
 
         Args:
             param1 (obj): self
-            param2 (str): base currency
+            param2 (bool): calculate from KZT?
             param3 [str]: target currency list
             param4 (dict): KZT exchange rates dictionary
 
         Returns:
-            A dicionary with currency rates for the provided currency.
+            A dicionary with currency rates for or from Kazakhstani Tenge.
 
             {
                 str(currency code): float(exchange rate), ...
             }
         """
         if not target_list:
-            target_list = [cur for cur in rates if cur != base]
-        if base == self.KZT_CODE:
+            target_list = [cur for cur in rates if cur != self.KZT_CODE]
+        if not from_kzt:
             return dict([(targ, 1 / rates[targ]) for targ in target_list])
-        return dict(
-                [(targ, self._calculate_through_kzt(
-                    base, targ, rates)) for targ in target_list])
+        return dict([(targ, rates[targ]) for targ in target_list])
 
-    def get_exchange_rates(self, base='KZT', currency_list=[], date=None):
-        """Method for gettings currency exchange rates for
-        Kazakhstani Tenge, KZT.
+    def get_exchange_rates(self, targets=[], from_kzt=False, date=None):
+        """Method for gettings currency exchange rates for or from
+        Kazakhstani Tenge.
 
         Args:
             param1 (obj): self
-            param2 (str): date string in format dd.mm.YYYY
-            param3 (str): 3-letter currency code
-            param4 (list): list of 3-letter currency code strings
+            param2 (list): list of 3-letter currency code strings
+            param3 (bool): calculate from KZT?
+            param4 (str):  string in format dd.mm.YYYY
 
         Returns:
-            A dicionary with currency rates for the provided currency,
+            A dicionary with currency rates for or from Kazakhstani Tenge,
             for the provided date or for the current moment, if no
             date was provided.
 
             {
                 'rates': {str(currency code): float(exchange rate), ...},
-                'base_currency': 'KZT',
                 'date': YYYY-mm-dd,
             }
 
         Examples:
-            >>> api.get_exchange_rates()
-            {'rates': {'AUD': 257.11, GBP: 511.29 ...}, base_currency: 'KZT',
-            'date': '2020-03-12'}
-            >>> api.get_exchange_rates(date='12.01.2019', base_currency='USD',
-                    currency_list=['KZT', 'EUR', 'GBP'])
-            {'rates': {'KZT': 393.4, 'EUR': 1.12, 'GBP': 1.56},
-             'base_currency': 'USD', 'date': '2019-01-12'}
+            >>> rates.get_exchange_rates()
+            {'rates': {'AUD': 0.0038047407069208236,
+                       'GBP': 0.0019146817798881828, ...},
+             'date': '2020-03-26'}
+            >>> rates.get_exchange_rates(from_kzt=True)
+            {'rates': {'AUD': 262.83, 'GBP': 522.28, ...},
+             'date': '2020-03-26'}
         """
+        # fetch data / get cached data
         url = self._get_rss_url(date)
         fetched_data = self._fetch_rates(url, date)
         rates = self._parse_rates_from_rss(fetched_data['rss'])
@@ -164,32 +140,30 @@ class Rates(object):
             res_date = fetched_data['rss'].find('date').text
         else:
             res_date = fetched_data['date'].strftime('%Y-%m-%d')
-        res_rates = self._get_rates_for_currency(
-            base, currency_list, rates)
-        return {'rates': res_rates, 'base_currency': base,
-                'date': res_date}
+        # get rates
+        res_rates = self._get_rates_for_targets(from_kzt, targets, rates)
+        return {'rates': res_rates, 'date': res_date}
 
-    def get_exchange_rate(self, base, target, date=None):
-        """Method to get exchange rate for a given currency
-        for a given date.
+    def get_exchange_rate(self, target, from_kzt=False, date=None):
+        """Method to get exchange rate for or from Kazakhstani Tenge.
 
         Args:
             param1 (obj): self
-            param2 (str): date string in format dd.mm.YYYY
-            param3 (str): 3-letter currency code
-            param4 (str): 3-letter currency code
+            param2 (str): 3-letter currency code
+            param3 (str): calculate from KZT?
+            param4 (str): date string in format dd.mm.YYYY
 
         Returns:
             (float): currency rate
 
         Examples:
-            >>> api.get_exchange_rate()
-            394.55
-            >>> api.get_exchange_rate(date='12.01.2019',
-                                       base_currency='USD', target='EUR')
-            0.897
+            >>> api.get_exchange_rate('USD')
+            0.0022482014388489208
+            >>> api.get_exchange_rate('USD', from_kzt=True)
+            444.8
+            >>> api.get_exchange_rate('USD', from_kzt=True, date='12.01.2019')
+            380.44
         """
-        res = self.get_exchange_rates(date=date,
-                                      base=base,
-                                      currency_list=[target])
+        res = self.get_exchange_rates(date=date, from_kzt=from_kzt,
+                                      targets=[target])
         return res['rates'][target]
